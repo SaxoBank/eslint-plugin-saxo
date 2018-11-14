222194b63ac9f60df2e6d36d380e198ddccee933
Julian Møller <jumoel@users.noreply.github.com>
2018-11-14 10:46:59 +0100

    refactor: remove lodash from entrypoint
    Uses vanilla `Array.prototype.reduce` instead of `lodash.zipObject`.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@
 
 const fs = require('fs');
 const path = require('path');
-const zipObject = require('lodash.zipobject');
 
-const rules = fs.readdirSync(path.resolve(__dirname, 'rules')).map((f) => f.replace(/\.js$/, ''));
+const rules = fs
+    .readdirSync(path.resolve(__dirname, 'rules'))
+    .map(f => f.replace(/\.js$/, ''));
 
 module.exports = {
-    rules: zipObject(rules, rules.map((rule) => require(`./rules/${rule}`))),
+    rules: rules.reduce((ruleObj, rule) =>
+        Object.assign(ruleObj, { [rule]: require(`./rules/${rule}`) })
+    ),
     configs: {
         recommended: {
             rules: {
@@ -16,8 +19,8 @@ module.exports = {
                 '@saxo/saxo/jsx-conditional-parens': 'error',
                 '@saxo/saxo/jsx-curly-spacing-opinionated': 'error',
                 '@saxo/saxo/jsx-enforce-prop-usage': 'warn',
-                '@saxo/saxo/jsx-enforce-spec-describe': 'warn',
-            },
-        },
-    },
+                '@saxo/saxo/jsx-enforce-spec-describe': 'warn'
+            }
+        }
+    }
 };
