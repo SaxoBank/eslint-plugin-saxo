4fb469c633e461e68faf072debb3b808340f893c
Jakub Sadura <spam@sadura.pl>
2019-03-15 16:37:51 +0100

    npm run lint-fix

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ const path = require('path');
 
 const rules = fs
     .readdirSync(path.resolve(__dirname, 'rules'))
-    .map(f => f.replace(/\.js$/, ''));
+    .map((f) => f.replace(/\.js$/, ''));
 
 module.exports = {
     rules: rules.reduce((ruleObj, rule) =>
         Object.assign(ruleObj, { [rule]: require(`./rules/${rule}`) })
-    , {}),
+        , {}),
     configs: {
         recommended: {
             rules: {
@@ -19,8 +19,8 @@ module.exports = {
                 '@saxo/saxo/jsx-conditional-parens': 'error',
                 '@saxo/saxo/jsx-curly-spacing-opinionated': 'error',
                 '@saxo/saxo/jsx-enforce-prop-usage': 'warn',
-                '@saxo/saxo/jsx-enforce-spec-describe': 'warn'
-            }
-        }
-    }
+                '@saxo/saxo/jsx-enforce-spec-describe': 'warn',
+            },
+        },
+    },
 };
