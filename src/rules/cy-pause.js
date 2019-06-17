defc89e47c54e409aca2d6ac6b4b4797c689e76d
Rob Pilling <bobrippling@users.noreply.github.com>
2019-06-17 08:39:31 +0100

    Be more thorough checking cy.pause() (#11)
    We now check for member expressions  e.g. `cy.member.pause()` as
    well as `cy.member().pause()`. This comes as a side effect of how
    we now walk an expression tree.
    
    Walking the expression tree was changed to fix a few cases where
    we were accessing values that weren't necessarily available on the
    expression 'union'.

--- src/rules/cy-pause.js
+++ src/rules/cy-pause.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { getObjectName } = require('../astUtils');
+const { firstMemberExpression } = require('../astUtils');
 
 module.exports = {
     meta: {
@@ -12,12 +12,52 @@ module.exports = {
     create(context) {
         return {
             ExpressionStatement(node) {
-                if (node.expression.callee.type === 'MemberExpression' && node.expression.callee.property.name === 'pause' && getObjectName(node.expression.callee) === 'cy') {
-                    context.report({
-                        node,
-                        message: 'Do not use cy.pause()',
-                    });
+                // <something>()
+                //            ^~
+                if (node.expression.type !== 'CallExpression') {
+                    return;
                 }
+
+                // <something>.target()
+                // ^~~~~~~~~~~~~~~~~~
+                const { callee } = node.expression;
+                if (callee.type !== 'MemberExpression') {
+                    return;
+                }
+
+                // <something>.pause()
+                //             ^~~~~
+                const { property: { name: memberName } } = callee;
+                if (memberName !== 'pause') {
+                    return;
+                }
+
+                // object.member().member.pause()
+                // ^~~~~~
+                const dotStart = firstMemberExpression(callee);
+                if (!dotStart) {
+                    return;
+                }
+
+                // object.member.pause()
+                // ^~~~~~ accept
+                // (b ? x : y).member.pause()
+                // ^~~~~~~~~~~ ignore
+                if (dotStart.type !== 'Identifier') {
+                    return;
+                }
+
+                // cy.pause()
+                // ^~ accept
+                const { name: objectName } = dotStart;
+                if (objectName !== 'cy') {
+                    return;
+                }
+
+                context.report({
+                    node,
+                    message: 'Do not use cy.pause()',
+                });
             },
         };
     },
